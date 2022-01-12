cbaca62755083f946837119b22c08383ed74730d
yunuscoder <57803311+yunuscoder@users.noreply.github.com>
2022-01-12 19:24:04 +0300

    SohbetUygulaması
    C# kullanılarak hazırlanmış desktop Client Server chat uygulaması

---
+++ Karşılıklı Mesajlaşma Uygulaması/server/serverChat/serverChat/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("serverChat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("serverChat")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("9c662de9-4ab9-42af-b6d4-d1a680f96a4d")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
